9d8e15f9b2f4c3533538d51d55a18dfad3f35cce
GhostQueen072 <72121168+GhostQueen072@users.noreply.github.com>
2021-04-23 02:48:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+	require_once "controlador/CtrlPrincipal.php";
+		require_once "controlador/CtrlUsuario.php";
+
+	require_once "modelo/MdlUsuario.php";
+
+
+	$Obj_Principal = new Principal();
+		$Obj_Principal -> CtrlPrincipal();
